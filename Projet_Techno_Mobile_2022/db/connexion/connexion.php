3e9fdc1cc0ea327f383e72e5da19fe60363efd1e
VicGuillaume <vicoxtreme@gmail.com>
2022-03-28 20:45:39 +0200

    J'ai fais la connexion à la bdd

--- Projet_Techno_Mobile_2022/db/connexion/connexion.php
+++ Projet_Techno_Mobile_2022/db/connexion/connexion.php
@@ -1,37 +1,37 @@
 <?php
 
-// Normalement ok
-
 require_once 'configurationBDO.php';
 
 $conn = connectDB();
 try {
-
-    $firstname = $_POST["firstname"];
+    $mail = $_POST["mail"];
     $passwd = $_POST["passwd"];
 
     // sql 
-    $sql = "Select * from user where nom=:firstname";
+    $sql = "select * from user where mail=:mail";
   
     // Prepare statement
     $stmt = $conn->prepare($sql);
 
-    $stmt->bindParam(':firstname', $firstname, PDO::PARAM_STR, 50);
+    $stmt->bindParam(':mail', $mail, PDO::PARAM_STR, 50);
     
     $stmt->execute();
 
     $rs = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-    $hashed_password = $rs[0]['MDP'] ;
+    if (!empty($rs)) {
+        $hashed_password = $rs[0]['passwd'];
 
-    if(password_verify($passwd, $hashed_password)){
-        session_start();
-        $_SESSION['firstname'] = $firstname ;
-        $_SESSION['statut'] = $rs[0]["GESTION"] ;
-        echo $_SESSION['statut'] ;
-    }
-    else{
-        echo -1 ;
+        if(password_verify($passwd, $hashed_password)){
+            session_start();
+            $_SESSION['mail'] = $mail;
+            echo "Connexion réussie !";
+        } else {
+            // TODO : Afficher mauvais mdp
+            echo "Connexion échouée ! (mauvais mdp)";
+        }
+    } else {
+        echo "Connexion échouée ! (mauvais mail)";
     }
     
 } catch(PDOException $e) {
--- Projet_Techno_Mobile_2022/db/connexion/session.php
+++ Projet_Techno_Mobile_2022/db/connexion/session.php
@@ -1,8 +1,7 @@
 <?php
 
 function isConnected(){
-    if(!isset($_SESSION['firstname'])) {
-        session_start();
+    if(!isset($_SESSION['mail'])) {
         header("Location: app/pages/home.html");
     } else {
         // TODO Afficher la page de l'utilisateur
