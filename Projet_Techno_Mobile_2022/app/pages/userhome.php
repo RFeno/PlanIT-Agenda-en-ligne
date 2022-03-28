ae931eedab8f178583053772366fcf07d29aa8c2
radam Adamaheto <skaytroz@gmail.com>
2022-03-28 10:25:16 +0200

    ajout footer

---
+++ Projet_Techno_Mobile_2022/app/pages/userhome.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="fr" class="scroll-smooth">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="app/assets/css/animate.css" />
+    <link rel="stylesheet" href="app/assets/css/tailwind.css" />
+    <link rel="shortcut icon" href="app/images/logo.png" type="image/x-icon"/>
+    <script src="app/assets/js/wow.min.js"></script>
+    <title>PlanIT - Accueil</title>
+</head>
+<body class="bg-blue-500">
+    
+
+  
+ 
+  <!-- fin main -->
+
+  
+  <!-- debut footer-->
+  <footer class="p-4 rounded-lg shadow md:flex md:items-center md:justify-between md:p-6">
+    <span class="text-sm text-blue-50 sm:text-center dark:text-blue-50">© 2022 <a href="https://rfeno.fr" class="hover:underline">PlanIT™</a>. Tous les droits réservés.
+    </span>
+    <ul class="flex flex-wrap items-center mt-3 text-sm text-blue-50 dark:text-blue-500 sm:mt-0">
+        <li>
+            <a href="#about" class="mr-4 hover:underline md:mr-6 ">About</a>
+        </li>
+
+        <li>
+            <a href="#contact" class="hover:underline">Contact</a>
+        </li>
+    </ul>
+  </footer>
+  <!--fin footer-->
+
+  <!-- scripy-->
+  <script src="app/assets/js/main.js"></script>
+
+</body>
+</html>
